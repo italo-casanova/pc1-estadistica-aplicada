c75ee06f73eed874f36b49d1ef97b5d9c7a7b4af
Italo <italo.casanova.d@uni.pe>
2022-10-25 18:20:11 -0500

    update

--- results/process/unpack.py
+++ results/process/unpack.py
@@ -6,7 +6,16 @@ import csv
 def unpack(path_name: str) -> list:
     """unpack the data into a list of lists"""
     with open(path_name, newline='') as csvfile:
+
         reader = csv.reader(csvfile, delimiter=',')
         data = [row for row in reader]
     csvfile.close()
     return data
+
+
+
+with open("../../data/Software_Professional_Salaries.csv", newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    data = [row for row in reader]
+
+    print(data)
