dadd64f877542837f683aa595aef1cb8d0468815
4verBurga <ever.burga.p@uni.pe>
2022-10-25 20:58:44 -0500

    hyp1: ploteo de grafica y comparacion

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # Objetivos
 
-* Distribucion sigue la ley normal:
-* Relacion estado civil - ingresos => comparacion entre hombres casado y solteros mayores de 40
-* Relacion profesion - ingresos
-* Relacion educacion - ingresos
-* nacionalidad: los recidentes de Mexico ganan mas en promeido que los peruanos:
+* Distribucion sigue la ley normal: (ever)
+* Relacion estado civil - ingresos => comparacion entre hombres casado y solteros mayores de 40 (ever)
+* Relacion profesion - ingresos (Josue)
+* Relacion educacion - ingresos (Josue)
+* nacionalidad: los recidentes de Mexico ganan mas en promeido que los peruanos: (Cuyate)
     > um =
-* Comparar distribuciones con varianza
-* las personas con un promediod de horas trabajadas mayor a X tienen ingresos mayores a Y
-* Comparar Edad con ingresos
-* Comparar Tipo de trabajo con ingresos:
+* Comparar distribuciones con varianza 
+* las personas con un promediod de horas trabajadas mayor a X tienen ingresos mayores a Y 
+* Comparar Edad con ingresos (Cuyate)
+* Comparar Tipo de trabajo con ingresos: (Italo)
     > para conocer si existe una
 
 
 ## Finalidad
+
+
+https://www.kaggle.com/datasets/iamsouravbanerjee/software-professional-salaries-2022
+https://www.kaggle.com/datasets/nikhilbhathi/data-scientist-salary-us-glassdoor
 
 Prospectiva de tendencias economicas
--- results/hipotesis1/main.py
+++ results/hipotesis1/main.py
@@ -1,17 +1,34 @@
 import os, sys
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
 
+# setting the path to the root of the project
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import process.unpack as unpack
 import process.analyze as analyzer 
 import process.visualize as visualize
 
-lista_listas = unpack.unpack("data\data_science.csv")
-lista_columnas = list( enumerate(lista_listas[0]))
+data = unpack.unpack("data\data_science.csv")
+columnas = list( enumerate(data[0]))
 
 if __name__ == "__main__":
     print("Desarrollo de la primera hipótesis")
-    print(lista_columnas)
-    print(visualize.visualize_by_column_floats(lista_listas, 0, "*", 18))
-    plt.show()
+    print(columnas)
+
+    # "*" is as a wildcard
+    data_ingresos = analyzer.filter_by_column_floats(data, 0, "*", 18)
+
+    # remove the outliers that are 2.5 deviations of the median with numpy
+    data_ingresos = np.array(data_ingresos)
+    data_ingresos = data_ingresos[abs(data_ingresos - np.median(data_ingresos)) < 2.5 * np.std(data_ingresos)]
+    
+    # show in a histogram as probability density
+    plt.hist(data_ingresos, density=True)
+    
+    # display a normal distributions with the same mean and standard deviation of the data
+    plt.plot(np.linspace(np.min(data_ingresos), np.max(data_ingresos), 100), stats.norm.pdf(np.linspace(np.min(data_ingresos), np.max(data_ingresos), 100), np.mean(data_ingresos), np.std(data_ingresos)))
+    
+    # render the histograms
+    plt.show()  
     
