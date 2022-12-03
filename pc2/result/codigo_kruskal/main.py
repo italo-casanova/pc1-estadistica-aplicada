e049c3ecbc1d7c5d262160582320199f0521267d
4verBurga <ever.burga.p@uni.pe>
2022-12-02 23:13:37 -0500

    kruskal-wallis

--- pc2/result/codigo_kruskal/main.py
+++ pc2/result/codigo_kruskal/main.py
@@ -14,7 +14,6 @@ data_kruskall = unpack.unpack("data//data_kruskal//06-01-2020.csv")
 
 print("Kruskal-Wallis Test")
 print("=====================================")
-print(list(enumerate(data_kruskall[0])))
 grupo_los_olivos = analyzer.select_by_column_qualitative(data_kruskall, 2, "Los Olivos Academia")
 grupo_comas = analyzer.select_by_column_qualitative(data_kruskall, 2, "Comas Academia")
 grupo_santa_beatriz = analyzer.select_by_column_qualitative(data_kruskall, 2, "Santa Beatriz Academia")
@@ -27,19 +26,38 @@ notas_villa_el_salvador = analyzer.select_column_floats(grupo_villa_el_salvador,
 
 
 # visualize the data with plt
-print(stats.kruskal(notas_comas, notas_santa_beatriz, notas_los_olivos))
+print("Todos:")
+print(stats.kruskal(notas_comas, notas_santa_beatriz, notas_villa_el_salvador, notas_los_olivos))
+print("Comas, Santa Beatriz, Los Olivos: ")
+print(stats.kruskal(notas_comas, notas_santa_beatriz, notas_los_olivos)) # p = 0.0186
+print("Comas, Santa Beatriz, Villa el Salvador: ")
 print(stats.kruskal(notas_comas, notas_santa_beatriz, notas_villa_el_salvador))
 
 # plot the graphs in the same figure with subplots
 fig, axs = plt.subplots(2, 2)
 fig.suptitle('Kruskal-Wallis Test')
-axs[0, 0].hist(notas_comas)
+axs[0, 0].hist(notas_comas,bins = 5)
 axs[0, 0].set_title('Comas')
-axs[0, 1].hist(notas_santa_beatriz)
+axs[0, 1].hist(notas_santa_beatriz, bins = 5)
 axs[0, 1].set_title('Santa Beatriz')
-axs[1, 0].hist(notas_los_olivos)
+axs[1, 0].hist(notas_los_olivos, bins = 5)
 axs[1, 0].set_title('Los Olivos')
-axs[1, 1].hist(notas_villa_el_salvador)
+axs[1, 1].hist(notas_villa_el_salvador, bins = 5)
 axs[1, 1].set_title('Villa el Salvador')
 
+# plot the graphs as boxplots int he same figure with the same scale
+fig, axs = plt.subplots(2, 2)
+fig.suptitle('Kruskal-Wallis Test')
+axs[0, 0].boxplot(notas_comas)
+axs[0, 0].set_title('Comas')
+axs[0, 1].boxplot(notas_santa_beatriz)
+axs[0, 1].set_title('Santa Beatriz')
+axs[1, 0].boxplot(notas_los_olivos)
+axs[1, 0].set_title('Los Olivos')
+axs[1, 1].boxplot(notas_villa_el_salvador)
+axs[1, 1].set_title('Villa el Salvador')
+axs[0,0].set_ylim([0, 500])
+axs[0,1].set_ylim([0, 500])
+axs[1,0].set_ylim([0, 500])
+axs[1,1].set_ylim([0, 500])
 plt.show()
